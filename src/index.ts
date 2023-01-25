6d334c558be98349404fce27b5d9c8d9a09a87bb
mino <dev@kagabase.com>
2023-01-25 14:19:48 +0800

    Add help option

--- src/index.ts
+++ src/index.ts
@@ -4,13 +4,30 @@ import {getChangelog} from './changelog';
 export interface Options {
 	compare?: boolean;
 	hash?: boolean;
+	help?: boolean;
 }
+
+const HELP = `
+git-changelog
+
+git changelog [from] [to]
+
+Options
+
+--compare		include vertion compare url
+--hash			include commit url
+`;
 
 export default async function main(args: string[]) {
 	const argv = parser<Options>(args);
 
 	const [from, to] = argv._;
-	const {compare = true, hash = true} = argv;
+	const {compare = true, hash = true, help} = argv;
+
+	if (help) {
+		console.log(HELP);
+		return;
+	}
 
 	const changelog = getChangelog({
 		from,
